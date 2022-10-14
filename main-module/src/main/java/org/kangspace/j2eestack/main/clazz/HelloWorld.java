a337c9eaedecf918e58bcae54a67092daf49eebb
Will Kang <kango2gler@gmail.com>
2022-10-14 23:23:22 +0800

    add javac example

---
+++ main-module/src/main/java/org/kangspace/j2eestack/main/clazz/HelloWorld.java
@@ -0,0 +1,9 @@
+package org.kangspace.j2eestack.main.clazz;
+
+/**
+ * HelloWorld
+ * @author kangxuefeng
+ * @since 2022/10/14
+ */
+public class HelloWorld {
+}
---
+++ main-module/src/main/java/org/kangspace/j2eestack/main/clazz/Pong.java
@@ -0,0 +1,8 @@
+package org.kangspace.j2eestack.main.clazz;
+
+/**
+ * @author kangxuefeng
+ * @since 2022/10/14
+ */
+public class Pong {
+}
